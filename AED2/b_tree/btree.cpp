de363b93cd712b848a8b141fc12b71c1907a14f1
math-freire <matheus.freire34@hotmail.com>
2023-11-20 18:31:04 -0300

    Inserir em árvores B corrigido

--- AED2/b_tree/btree.cpp
+++ AED2/b_tree/btree.cpp
@@ -1,7 +1,24 @@
 #include <stdlib.h>
 #include "btree.h"
 
-btNo* btCriarNo(const unsigned char ordem);
+btNo* btCriarNo(const unsigned char ordem){
+    btNo *no = (btNo*) malloc(sizeof(btNo));
+    no -> ehFolha = 1; // is leaf at the start
+    no -> numChaves = 0;
+    no -> chaves = (int*) malloc(sizeof(int) * ordem);
+    no -> filhos = (btNo**) malloc(sizeof(btNo*) * (ordem+1));
+
+    if (no->chaves == NULL || no->filhos == NULL) {
+        // Tratamento de erro, caso a alocação falhe
+        free(no->chaves);
+        free(no->filhos);
+        free(no);
+        exit(EXIT_FAILURE);
+    }
+
+    return no;
+}
+
 bTree btCriar(const unsigned char ordem) {
     bTree b;
     b.ordem = ordem;
@@ -9,18 +26,8 @@ bTree btCriar(const unsigned char ordem) {
     return b;
 }
 
-btNo* btCriarNo(const unsigned char ordem){
-    btNo *no = (btNo*) malloc(sizeof(btNo));
-    no -> ehFolha = 1; // is leaf at the start
-    no -> numChaves = 0;
-    no -> chaves = (int*) malloc(sizeof(*no->chaves) * ordem);
-    no -> filhos = (btNo**) malloc(sizeof(*no->filhos) * (ordem+1));
-    return no;
-}
-
 void btDestruir(btNo *no);
 void btDestruir(bTree b) {
-    int i;
     if(b.raiz)
         btDestruir(b.raiz);
 }
@@ -29,7 +36,7 @@ void btDestruir(btNo *no){
         return;
 
     if(!no -> ehFolha){ // children first, therefore will access the leafs
-        for(int i = 0; i < no -> numChaves; i++)
+        for(int i = 0; i <= no -> numChaves; i++)
             btDestruir(no -> filhos[i]);  
     }
 
@@ -73,89 +80,86 @@ int btBuscar(bTree b, int chave) {
     return btBuscar(b.raiz, chave);
 }
 
-btNo *inserirInterno(btNo *b, int chave, int *mediana, const unsigned char ordem);
-void btInserir(bTree b, int chave) {
-    btNo *b1; // new left node
-    btNo *b2; // new right node
-    int mediana; // middle node (going up)
+btNo *insirirInterno(btNo *b, int chave, int *mediana, const unsigned char ordem);
+void btInserir(bTree b, int chave){
+    btNo *b1;
+    btNo *b2;
+    int mediana;
 
-    b2 = inserirInterno(b.raiz, chave, &mediana, b.ordem);
+    b2 = insirirInterno(b.raiz, chave, &mediana, b.ordem);
 
-    if(b2) { // root needs to divide: create new root
+    if(b2){
         b1 = btCriarNo(b.ordem);
         for(unsigned char i = 0; i < b.ordem; i++){
             b1 -> chaves[i] = b.raiz -> chaves[i];
-            b1 -> filhos[i] = b.raiz -> filhos[i];           
-        }
-
-        b1 -> filhos[b.ordem] = b.raiz -> filhos[b.ordem];
-
-        b1 -> ehFolha = b.raiz -> filhos[b.ordem];
-        b1 -> ehFolha = 0;
-
-        b1 -> numChaves = b.raiz -> numChaves;
-        b.raiz -> numChaves = 1;
-
-        b.raiz -> chaves[0] = mediana;
-        b.raiz -> filhos[0] = b1;
-        b.raiz -> filhos[1] = b2;
+            b1 -> filhos[i] = b.raiz -> filhos[i];
+        } b1 -> filhos[b.ordem] = b.raiz -> filhos[b.ordem];
+        b1 -> ehFolha = b.raiz->ehFolha; b.raiz -> ehFolha = 0;
+        b1 -> numChaves = b.raiz->numChaves; b.raiz -> numChaves = 1;
+        b.raiz->chaves[0] = mediana;
+        b.raiz->filhos[0] = b1;
+        b.raiz->filhos[1] = b2;
     }
 }
 
-btNo *inserirInterno(btNo *b, int chave, int *mediana, const unsigned char ordem){
+btNo *insirirInterno(btNo *b, int chave, int *mediana, const unsigned char ordem){
     int pos;
     int mid;
     btNo *b2;
 
-    pos = buscarChave(b -> numChaves, b -> chaves, chave); // get position on the current node
-    if(pos < b -> numChaves && b -> chaves[pos] == chave) // key already exist
+    pos = buscarChave(b->numChaves, b -> chaves, chave);
+    if(pos < b->numChaves && b->chaves[pos] == chave){
         return 0;
+    }
 
-    if(b -> ehFolha){  // found a leaf to insert. Put it on the right position
-        for(int i = b -> numChaves; i > pos; i--)
-            b -> chaves[i] = b -> chaves[i-1]; // All keys above 'pos' move a step up
-        b -> chaves[pos] = chave;
-        b -> numChaves++;
-    } else {
-        b2 = inserirInterno(b -> filhos[pos], chave, &mid, ordem);
+    if(b->ehFolha){
+        for(int i = b -> numChaves; i > pos; i--){
+            b->chaves[i] = b->chaves[i-1];
+        }
+        b->chaves[pos] = chave;
+        b->numChaves++;
+    } else{
+        b2 = insirirInterno(b->filhos[pos], chave, &mid, ordem);
         if(b2){
-            for(int i = b -> numChaves; i > pos; i--){
-                b -> chaves[i] = b -> chaves[i-1]; // All keys above 'pos' move a step up
-                b -> filhos[i+1] = b -> filhos[i]; // All children above 'pos' move a step up
+            for(int i = b ->numChaves; i > pos; i--){
+                b->chaves[i] = b ->chaves[i-1];
+                b->filhos[i+1]=b->filhos[i];
             }
-            b -> chaves[pos] = mid;
-            b -> filhos[pos + 1] = b2;
-            b -> numChaves++;
+            b->chaves[pos] = mid;
+            b->filhos[pos+1] = b2;
+            b->numChaves++;
         }
     }
 
-    if(b->numChaves >= ordem){ // verify if it needs to divide b
-        mid = b -> numChaves/2; // mediana index
-        *mediana = b -> chaves[mid]; // copy mediana value to be used on the father function
-        b2 = btCriarNo(ordem); // new leaf
-        b2 -> numChaves = b -> numChaves - mid - 1;
-        b2 -> ehFolha = b -> ehFolha;
+    if(b->numChaves >= ordem){
+        mid = b -> numChaves/2;
+        *mediana = b ->chaves[mid];
+        b2 = btCriarNo(ordem);
+        b2 -> numChaves = b->numChaves - mid - 1;
+        b2->ehFolha = b->ehFolha;
 
-        for(int i = mid+1; i < b -> numChaves; i++) // copy half from b to b2
-            b2 -> chaves[i-(mid+1)] = b -> chaves[i]; // copy half from b to b2
-        
-        if(!b -> ehFolha) // if not leaf, copy children too
-            for(int i = mid+1; i < b -> numChaves+1; i++) // every children above 'pos' move a step up
+        for(int i = mid + 1; i<b->numChaves; i++){
+             b2 -> chaves[i-(mid+1)] = b -> chaves[i];
+        }
+        if(!b->ehFolha){
+            for(int i = mid+1; i < b->numChaves+1; i++){
                 b2 -> filhos[i-(mid+1)] = b -> filhos[i];
-        
-        b -> numChaves = mid; // now b have half the elements
+            }
+        }
+
+        b -> numChaves = mid;
         return b2;
-    } 
-    else
+    } else {
         return 0;
+    }
 }
 
 
 int btAltura(bTree b) {
-    int count = 1;
+    int count = 0;
     btNo *cursor = b.raiz;
     
-    while(cursor -> numChaves){
+    while(cursor){
         count++;
         cursor = cursor -> filhos[0];
     }
@@ -164,13 +168,31 @@ int btAltura(bTree b) {
 }
 
 int btContaNos(bTree b) {
-    // IMPLEMENTAR!
-    // DICA: Implemente de modo recursivo
-    return 0;
+    if (!b.raiz)
+        return 0;
+
+    int count = 0;
+    btNo *cursor = b.raiz;
+
+    while (cursor) {
+        count += cursor->numChaves + 1; // Conta o nó atual e seus filhos
+        cursor = cursor->filhos[0];
+    }
+
+    return count;
 }
 
 int btContaChaves(bTree t) {
-    // IMPLEMENTAR!
-    // DICA: Muito similar a funcao btContaNos
-    return 0;
+    if (!t.raiz)
+        return 0;
+
+    int count = 0;
+    btNo *cursor = t.raiz;
+
+    while (cursor) {
+        count += cursor->numChaves; // Conta as chaves no nó atual
+        cursor = cursor->filhos[0];
+    }
+
+    return count;
 }
